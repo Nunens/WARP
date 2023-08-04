d39a6d33519427859990bb1519c76cf7f8c9deee
Stephen Molefe Kobue <stephen.kobue@zapper.com>
2023-08-04 16:30:05 +0200

    Removed commented code

--- app/src/main/java/com/devnunens/warp/data/entity/Data.kt
+++ app/src/main/java/com/devnunens/warp/data/entity/Data.kt
@@ -9,7 +9,6 @@ data class Data(
 data class Features(
     @SerializedName("properties") var properties: Properties? = Properties()
 )
-//event name "event", start date "effective", end date "ends", source "senderName" & duration (start date - end date)
 data class Properties(
     @SerializedName("id") var id: String? = null,
     @SerializedName("effective") var effective: String? = null,
@@ -17,31 +16,3 @@ data class Properties(
     @SerializedName("event") var event: String? = null,
     @SerializedName("senderName") var senderName: String? = null,
 )
-
-/*
-data class Properties(
-    @SerializedName("@id") var atId: String? = null,
-    @SerializedName("@type") var atType: String? = null,
-    @SerializedName("id") var id: String? = null,
-    @SerializedName("areaDesc") var areaDesc: String? = null,
-    @SerializedName("affectedZones") var affectedZones: ArrayList<String> = arrayListOf(),
-    @SerializedName("references") var references: ArrayList<String> = arrayListOf(),
-    @SerializedName("sent") var sent: String? = null,
-    @SerializedName("effective") var effective: String? = null,
-    @SerializedName("onset") var onset: String? = null,
-    @SerializedName("expires") var expires: String? = null,
-    @SerializedName("ends") var ends: String? = null,
-    @SerializedName("status") var status: String? = null,
-    @SerializedName("messageType") var messageType: String? = null,
-    @SerializedName("category") var category: String? = null,
-    @SerializedName("severity") var severity: String? = null,
-    @SerializedName("certainty") var certainty: String? = null,
-    @SerializedName("urgency") var urgency: String? = null,
-    @SerializedName("event") var event: String? = null,
-    @SerializedName("sender") var sender: String? = null,
-    @SerializedName("senderName") var senderName: String? = null,
-    @SerializedName("headline") var headline: String? = null,
-    @SerializedName("description") var description: String? = null,
-    @SerializedName("instruction") var instruction: String? = null,
-    @SerializedName("response") var response: String? = null,
-)*/
