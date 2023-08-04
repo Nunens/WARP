e2c62a4af8fcddab344c30d3afa1c1fed78d31c0
Stephen Molefe Kobue <stephen.kobue@zapper.com>
2023-08-04 16:33:17 +0200

    Removed commented code

--- app/src/main/java/com/devnunens/warp/domain/dto/WeatherAlertDTO.kt
+++ app/src/main/java/com/devnunens/warp/domain/dto/WeatherAlertDTO.kt
@@ -1,6 +1,5 @@
 package com.devnunens.warp.domain.dto
 
-//event name "event", start date "effective", end date "ends", source "senderName" & duration (start date - end date)
 data class WeatherAlertDTO(
     val startDate: String,
     val endDate: String,
